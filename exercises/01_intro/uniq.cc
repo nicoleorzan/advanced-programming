ba8c0a8a011aaa5633abd3f19048a2d3617a6143
nicoleorzan <orzan.nicole@gmail.com>
2017-10-12 17:02:05 +0200

    ultimo esercizio messo

---
+++ exercises/01_intro/uniq.cc
@@ -0,0 +1,25 @@
+using namespace std;
+#include <iostream>
+#include <string>
+
+int main(){
+  int i=1;
+  string line, line1;
+
+  getline(cin,line);
+
+  while(getline(cin,line1)){
+    if (line==line1){
+      i++;
+      line=line1;
+    }
+    else{
+      cout<<i<<" "<<line<<endl;
+      i=1;
+      line=line1;
+    }
+}
+  cout<<i<<" "<<line<<endl;
+   
+  return 0;
+}
