cd5e7ab757444bafe5f6b45e1829fa309a939e3e
nicoleorzan <orzan.nicole@gmail.com>
2017-11-30 13:45:19 +0100

    going on with the date exercise

--- exercises/03_classes/class_date.cc
+++ exercises/03_classes/class_date.cc
@@ -54,7 +54,8 @@ void dwim(Month c) {
 
 class Date{
 
-  unsigned int _d, _y;
+  unsigned int _d;
+  unsigned int _y;
   Month _m;
   
   void add_singleday(){ _d+=1; }
@@ -70,13 +71,25 @@ public:
   }
   ~Date(){};//qui non ho capito perche` senza le graffe mi da errore
   
-  const unsigned int get_day(){return _d;} //const davanti?
-  const Month get_month(){ return _m;}
-  const unsigned int get_year(){return _y;}
+  unsigned int get_day() const {return _d;}
+  Month get_month() const { return _m;}
+  unsigned int get_year() const {return _y;}
   
   void add_days(const unsigned int n);
   
   };
+
+bool operator==(const Date& lhs, const Date& rhs){
+  return ((lhs.get_day()==rhs.get_day()) && (lhs.get_month()==rhs.get_month()) && (lhs.get_year()==rhs.get_year()));
+}
+
+bool operator!=(const Date& lhs, const Date& rhs){
+  return ((lhs.get_day()!=rhs.get_day()) && (lhs.get_month()!=rhs.get_month()) && (lhs.get_year()!=rhs.get_year()));
+}
+
+std::ostream& operator<<(std::ostream& os, const Date& d){
+  return (os << d.get_day() <<", " << int(d.get_month()) << ", " <<d.get_year() );
+}
 
 std::istream& operator>>(std::istream& is, Month& mese){
     int a;
@@ -88,7 +101,8 @@ std::istream& operator>>(std::istream& is, Month& mese){
 
 int main(){
 
-  unsigned int day, year;
+  unsigned int day;
+  unsigned int year;
   Month mese;// = Month::feb;
   cout<<"giorno? ";
   cin>>day;
@@ -96,13 +110,14 @@ int main(){
   std::cin >> mese;
   cout<<"anno? ";
   cin>>year;
-
+  Date data(day,mese,year);
   //int a = int(mese);
   
-  cout<<"Data: giorno "<<day;
-  dwim(mese);
-  cout<<", anno "<<year<<endl;
-  Date data(day,mese,year);
+  cout<<"Data inserita:"<<endl;
+  std::cout << data << '\n';
+  //dwim(mese);
+  //cout<<", anno "<<year<<endl;
+
 
   unsigned int numero;
   cout<<"inserire giorni da aggiungere: ";
@@ -110,12 +125,15 @@ int main(){
   const unsigned int n=numero;
   cout<<"dopo "<<n<<" giorni sara` il ";
   data.add_days(n);
-  cout<<data.get_day();
+  std::cout << data << '\n';
+    
+  
   //int a2 = int(data.get_month());
   //cout<<", mese "<<a2;
+  /*cout<<data.get_day();
   dwim(data.get_month());
-  cout<<", anno "<<data.get_year()<<endl;;
-
+  cout<<", anno "<<data.get_year()<<endl;*/
+  
     return 0;
   }
 
@@ -143,8 +161,13 @@ void Date::add_days(const unsigned int n){
 	if (_d<31) Date::add_singleday();
 	else if(_d==31){
 	  _d=1;
-	  int temp=int(_m)+1;
-	  _m = (Month)temp;
+	  if(int(_m)!=12){
+	    int temp=int(_m)+1;
+	    _m = (Month)temp;
+	  }else{
+	    _m = (Month)1;
+	    _y+=1;
+	  }
 	}
       }
     }
@@ -153,8 +176,14 @@ void Date::add_days(const unsigned int n){
 	if (_d<29) Date::add_singleday();
 	else if(_d==29){
 	  _d=1;
-	  int temp=int(_m)+1;
-	  _m = (Month)temp;
+	  if(int(_m)!=12){
+	    int temp=int(_m)+1;
+	    _m = (Month)temp;
+	  }else{
+	    _m = (Month)1;
+	    _y+=1;
+	  }
+
 	}
       }
     }
